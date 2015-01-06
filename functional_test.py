60a56195f886635c79e4d778989ba194be90f323
Scott Chen <scott8312@yahoo.com>
2015-01-06 09:12:17 -0500

    refactor FT

--- functional_test.py
+++ functional_test.py
@@ -9,7 +9,12 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
- 
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # check the home page
         self.browser.get('http://localhost:8000')
@@ -38,17 +43,11 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER) 
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows]) 
-        self.assertIn('2: Use peacock feathers to make a fly',
-                       [row.text for row in rows]
-        )
-
-        # still a text box inviting to add another item
-        self.fail('Finish the test!') 
-
         # the page updates again, now both items on the list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        self.fail('Finish the test!') 
 
         # will the site remembers the list?
 
