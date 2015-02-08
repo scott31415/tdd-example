9620120d4ae3079a1c4b518bebeb4076a764e921
Scott Chen <scott8312@yahoo.com>
2015-02-07 23:04:30 -0500

    make functional_tests an app, use LiveServerTestCase

---
+++ functional_tests/test.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
+
+class NewVisitorTest(LiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # check the home page
+        self.browser.get(self.live_server_url)
+        
+        # the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        ) 
+
+        # type "buy point cards" into a text box
+        inputbox.send_keys('Buy peacock feathers')
+
+        # when hitting enter, the page updates
+        inputbox.send_keys(Keys.ENTER)
+
+        # there is still a text box inviting her to add another item
+	# she enters "Use peacock feathers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER) 
+
+        # the page updates again, now both items on the list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        self.fail('Finish the test!') 
+
+        # will the site remembers the list?
+
+        # visit the URL and check the list is still there
+
+        # happy
+
+
+
+
+
