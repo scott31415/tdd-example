dbcdba37773f78112c5c0d2b7065bbf6fbb666d5
Scott Chen <scott8312@yahoo.com>
2015-02-10 09:29:12 -0500

    Hack FT runner to be able to test staging

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,8 +1,23 @@
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 class NewVisitorTest(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url 
+
+    @classmethod
+    def tearDownClass(cls): 
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -16,7 +31,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn(row_text,[row.text for row in rows])
 
     def test_layout_and_styling(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024,768)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -36,7 +51,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # check the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         
         # the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -76,7 +91,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
         
         # Francis visits the page and there is no sign of Edith's list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
